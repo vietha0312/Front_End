df50ced055b2fbd11eea0c506f33bac93af9cfdf
vietha0312 <pvietha301@gmail.com>
2023-06-04 15:13:49 +0700

    feat: create-base fefs:AA-16

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ProductListComponent } from './components/product-list/product-list.component';
+import { UserComponent } from './layout/user/user.component';
+import { AdminComponent } from './layout/admin/admin.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductListComponent,
+    UserComponent,
+    AdminComponent
   ],
   imports: [
     BrowserModule
