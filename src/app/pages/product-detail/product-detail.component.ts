e2bb5f2583f850ca4031c77a24f111de052f7cad
NTienDuy <105479766+NTienDuy@users.noreply.github.com>
2023-06-07 17:17:02 +0700

    feat: create-product-detail-page refs: AA-23

--- src/app/pages/product-detail/product-detail.component.ts
+++ src/app/pages/product-detail/product-detail.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { IProduct } from 'src/app/interfaces/Product';
+import { ProductService } from 'src/app/Services/Product/product.service';
 
 @Component({
   selector: 'app-product-detail',
@@ -6,5 +9,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./product-detail.component.scss']
 })
 export class ProductDetailComponent {
-
+  product : IProduct = {
+    name: "",
+    price: 0
+  }
+  category: any = {}
+  constructor(
+    private route: ActivatedRoute,
+    private productService: ProductService
+  )
+  {
+    this.route.paramMap.subscribe(param => {
+      const id = Number(param.get('id'));
+      this.productService.getProductById(id).subscribe(product => {
+        this.product = product
+      })
+    })
+  }
+  ngOnInit(){
+    this.getCategory();
+  }
+  getCategory() {
+    this.productService.relatedProducts(1).subscribe(data => {
+      this.category = data
+    })
+  }
 }
