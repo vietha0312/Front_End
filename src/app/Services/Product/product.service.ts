73e938edd95d8c28b2c7f8e6a932518cf50fa3c5
minhnvph20397 <105479880+Zatuzick@users.noreply.github.com>
2023-06-11 18:17:40 +0700

    feat: Header-Authorization refs:AA-33

--- src/app/Services/Product/product.service.ts
+++ src/app/Services/Product/product.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { finalize } from 'rxjs/operators';
 import { IProduct, IResponse } from 'src/app/interfaces/Product';
@@ -9,7 +9,8 @@ import { IProduct, IResponse } from 'src/app/interfaces/Product';
 export class ProductService {
 
   constructor(private http: HttpClient) { }
-  private API_Url = `http://localhost:8080/api`;
+  API_Url = `http://localhost:8080/api`;
+  Token = JSON.parse(localStorage.getItem('userInfo') || '{}').accessToken;
 
 
   getProducts(): Observable<IResponse> {
@@ -25,7 +26,9 @@ export class ProductService {
     return this.http.put<IProduct>(`${this.API_Url}/products/${product._id}`, product)
   }
   deleteProduct(_id: string): Observable<IResponse> {
-    return this.http.delete<IResponse>(`${this.API_Url}/products/${_id}`)
+    return this.http.delete<IResponse>(`${this.API_Url}/products/${_id}`, {
+      headers: new HttpHeaders().set('Authorization', `Bearer ${this.Token}`)
+    })
   }
   relatedProducts(categoryId: number): Observable<any> {
     return this.http.get<any>(`${this.API_Url}/categories/${categoryId}?_embed=products`)
